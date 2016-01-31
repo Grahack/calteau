cdbb00accb5ecac523d7753e48a8554250cccaf1
Christophe Gragnic <christophegragnic@gmail.com>
2016-01-31 17:56:14 +0100

    Start using Codox

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject calteau "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Clojure lib to parse and convert source code from calculators"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.blancas/kern "0.7.0"]]
+  :profiles {:dev {:plugins [[lein-codox "0.9.1"]]}
+             :uberjar {:aot :all}}
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-  :aot [calteau.core]
+  :codox {:metadata {:doc "FIXME: write docs"
+                     :doc/format :markdown}
+          :source-uri "https://github.com/grahack/calteau/blob/{version}/{filepath}#L{line}"}
   :main calteau.core)
